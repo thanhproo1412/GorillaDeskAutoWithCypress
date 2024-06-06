58ca34a49e31d5297bc301b73df2ff7503a56a2f
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2024-06-06 16:40:35 +0700

    add report library

--- cypress.config.js
+++ cypress.config.js
@@ -6,13 +6,19 @@ module.exports = defineConfig({
   projectId: "hgwkwi",
   viewportWidth: 1920,
   viewportHeight: 1080,
-  
+
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
     },
   },
+
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporterConfig.json',
+  },
+
 
   e2e: {
     setupNodeEvents(on, config) {
