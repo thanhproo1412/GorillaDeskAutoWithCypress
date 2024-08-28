a4ba7a6d682ebf47e1a427fedf712e94c7fc839c
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2024-08-28 15:29:09 +0700

    update cypress/ test login

--- cypress/integration/tests/TC_001_LoginToGD.cy.js
+++ cypress/integration/tests/TC_001_LoginToGD.cy.js
@@ -1,16 +1,25 @@
 import LoginAction from '../pages/LoginPage/LoginAction';
 import privateData from '../../testData/PrivateData.json'; // Assuming login.json is in testData folder
 
+for (let i = 0; i < 1; i++) {
 
-describe('Login to GD', () => {
-  it('should Login to GD', () => {
+  describe('Login to GD', () => {
+    it('should Login to GD', () => {
 
-    // Nav to home page
-    cy.visit(privateData.url);
+      // Nav to home page
+      cy.visit(privateData.url);
 
-    // Nav to AB Testing page
-    const loginAction = new LoginAction();
-    loginAction.login(privateData.username, privateData.password)
+      // Nav to AB Testing page
+      const loginAction = new LoginAction();
+      loginAction.login(privateData.username, privateData.password)
 
+      cy.get('#title-calendar-time', { timeout: 30000 })
+
+      console.log(cy.getAllLocalStorage())
+
+    });
   });
-});
+
+}
+
+
