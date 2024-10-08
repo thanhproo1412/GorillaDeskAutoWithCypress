547894023da5cd8fa0b9eb788e965c72a012374c
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2024-10-08 23:15:12 +0700

    a

--- cypress/integration/tests/TC_F002_CreateNewCustomer.cy.js
+++ cypress/integration/tests/TC_F002_CreateNewCustomer.cy.js
@@ -6,10 +6,22 @@ import data from '../../testData/TC_F002_CreateNewCustomer.json';
 
 describe('Create Job', () => {
   it('Just create a new job in calendar', () => {
+
+    // Prepare Expected data??
+    const newCustomerData = {
+      "name": "Eliza",
+      "email": "eliza@example.com"
+    };
+    // Create Expected Json file
+    cy.writeFile('cypress/ExpectedData/Expected_TC_F002_CreateNewCustomer.json', newCustomerData)
+      .then(() => {
+        cy.log('File written successfully');
+      });
+
     // Navigate to home page
     cy.visit(privateData.url);
 
-    // Navigate to AB Testing page
+    // Login to GD
     const loginAction = new LoginAction();
     loginAction.login(privateData.username, privateData.password);
 
@@ -21,17 +33,7 @@ describe('Create Job', () => {
     const newCustomerAction = new NewCustomerAction();
     newCustomerAction.AddNewCustomer(data);
 
-    // Optionally, if you want to modify or use the data after adding the customer
-    const newCustomerData = {
-      // You can structure this object however you need
-      name: data.name, // Example of how to pull in data from the original object
-      // Add other customer details here as necessary
-    };
 
-    // Write the new customer data to a JSON file
-    cy.writeFile('../../expectedData/TC_F002_CreateNewCustomer.json', newCustomerData)
-      .then(() => {
-        cy.log('File written successfully');
-      });
+
   });
 });
